0e83d247fc0bcb00c2483d6720ec27151c194e63
AdiJainy <adij1222@gmail.com>
2023-06-22 20:41:52 +0530

    lec 2: added parcel, react and browserlist to the project

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", {id:"title1"}, "namaste yay");
 const heading2 = React.createElement("h1", {id:"itite2"}, "react");
 const div = React.createElement(

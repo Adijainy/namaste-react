0a8b426028a4a771f4a10e63403436a61e1bc260
AdiJainy <adij1222@gmail.com>
2023-06-24 08:16:35 +0530

    learnt about functional components and used them in my code also used components inside components and react elements inside components

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = <h1 id="h1"> namaste bro </h1>;
+const title1 =(
+    <h3>
+         react element inside function 
+    </h3>
+);
+const Title2 = ()=> (
+    <h3> 
+        function inside function with tag 
+    </h3>
+);
+const Title3 = ()=> (
+    <h3> 
+        function inside function, call the function
+    </h3>
+);
+//react functional componenet
+const Header = ()=> (
+    <div>
+        <h1> React functional  </h1>
+        <h2>component </h2>
+        {title1}
+        <Title2 />
+        {Title3()}
+    </div>  
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Header/>);

fc5339d373cb379162aad6aa67bc88b7afd489e4
AdiJainy <adij1222@gmail.com>
2023-06-24 07:17:03 +0530

    added jsx to my app.js file

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", {id:"title1"}, "namaste yay");
-const heading2 = React.createElement("h1", {id:"itite2"}, "react");
-const div = React.createElement(
-    "div", {}, [heading, heading2] 
-    )
-
+const heading = <h1 id="h1"> namaste bro </h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(div);
+root.render(heading);
